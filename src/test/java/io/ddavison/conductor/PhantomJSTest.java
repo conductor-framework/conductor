0d0498c5ee21550fac438779ae5a9c8d11f2198f
Daniel Davison <daniel.jj.davison@gmail.com>
2017-01-18 16:11:30 -0500

    create phantomjs test, add phantomjs log to gitignore

---
+++ src/test/java/io/ddavison/conductor/PhantomJSTest.java
@@ -0,0 +1,21 @@
+package io.ddavison.conductor;
+
+import org.junit.Test;
+
+/**
+ * Created by dj on 1/18/17.
+ */
+@Config(browser = Browser.PHANTOMJS, url="http://ddavison.io/tests/getting-started-with-selenium.htm")
+public class PhantomJSTest extends Locomotive {
+  @Test
+  public void testClick() throws Exception {
+    click("#click")
+    .validatePresent("#click.success"); // adds the .success class after click
+  }
+
+  @Test
+  public void testSetText() throws Exception {
+    setText("#setTextField", "test")
+    .validateText("#setTextField", "test");
+  }
+}
