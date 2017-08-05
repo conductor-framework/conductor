e6a9b1dcbda388a87eba4b1db76641dbbb188078
Daniel Davison <daniel.jj.davison@gmail.com>
2017-08-05 03:42:17 -0400

    prep for ci

--- src/test/java/io/ddavison/conductor/SafariTests.java
+++ src/test/java/io/ddavison/conductor/SafariTests.java
@@ -10,7 +10,7 @@
 package io.ddavison.conductor;
 
 @Config(browser = Browser.SAFARI)
-public class SafariTest extends FrameworkTest {
+public class SafariTests extends FrameworkTest {
     @Override
     public void testWindowSwitching() throws Exception {}
 }
